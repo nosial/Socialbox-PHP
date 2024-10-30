ac09674fdda16799a5f51e142436b5d258fe74d5
netkas <netkas@n64.cc>
2024-10-30 15:27:36 -0400

    Add username validation to Validator class

--- src/Socialbox/Classes/Validator.php
+++ src/Socialbox/Classes/Validator.php
@@ -4,7 +4,8 @@ namespace Socialbox\Classes;
 
 class Validator
 {
-    private const PEER_ADDRESS_PATTERN = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+    private const string PEER_ADDRESS_PATTERN = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+    private const string USERNAME_PATTERN = "/^[a-zA-Z0-9_]+$/";
 
     /**
      * Validates a peer address
@@ -16,4 +17,21 @@ class Validator
     {
         return preg_match(self::PEER_ADDRESS_PATTERN, $address) === 1;
     }
+
+    /**
+     * Validates a username
+     *
+     * @param string $username The username to validate.
+     * @return bool True if the username is valid, false otherwise.
+     */
+    public static function validateUsername(string $username): bool
+    {
+        if(strlen($username) < 3 || strlen($username) > 255)
+        {
+            return false;
+        }
+
+        return preg_match(self::USERNAME_PATTERN, $username) === 1;
+    }
+
 }
