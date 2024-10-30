3b55e550c5c95eea7e01d643898f3eda934e4646
netkas <netkas@n64.cc>
2024-10-30 15:28:52 -0400

    Add logging for RpcRequest response production

--- src/Socialbox/Objects/RpcRequest.php
+++ src/Socialbox/Objects/RpcRequest.php
@@ -4,6 +4,7 @@ namespace Socialbox\Objects;
 
 use InvalidArgumentException;
 use ncc\ThirdParty\nikic\PhpParser\Node\Expr\BinaryOp\BooleanOr;
+use Socialbox\Classes\Logger;
 use Socialbox\Enums\StandardError;
 use Socialbox\Exceptions\RpcException;
 use Socialbox\Exceptions\StandardException;
@@ -130,6 +131,7 @@ class RpcRequest implements SerializableInterface
             throw new InvalidArgumentException('The \'$result\' property must either be string, boolean, integer, array, null or SerializableInterface');
         }
 
+        Logger::getLogger()->verbose(sprintf('Producing response for request %s', $this->id));
         return new RpcResponse($this->id, $result);
     }
 
