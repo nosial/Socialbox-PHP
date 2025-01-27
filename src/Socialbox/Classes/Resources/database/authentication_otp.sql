23fe57d514cbf23dd2098bfa1a908fdad67b7109
netkas <netkas@n64.cc>
2025-01-27 03:27:58 -0500

    Oops

--- src/Socialbox/Classes/Resources/database/authentication_otp.sql
+++ src/Socialbox/Classes/Resources/database/authentication_otp.sql
@@ -1,18 +1,18 @@
-create table authentication_passwords
+create table authentication_otp
 (
-    peer_uuid varchar(36)                           not null comment 'The primary unique index of the peer uuid'
-        primary key,
-    hash      mediumtext                            not null comment 'The encrypted hash of the password',
-    updated   timestamp default current_timestamp() not null comment 'The Timestamp for when this record was last updated',
-    constraint authentication_passwords_peer_uuid_uindex
-        unique (peer_uuid) comment 'The primary unique index of the peer uuid',
-    constraint authentication_passwords_registered_peers_uuid_fk
+    peer_uuid varchar(36)                           not null comment 'The Peer UUID associated with this record'
+        primary key comment 'The Peer UUID unique Index',
+    secret    mediumtext                            not null comment 'The encrypted secret for the OTP',
+    updated   timestamp default current_timestamp() not null comment 'The Timestamp for when the record was last updated',
+    constraint authentication_otp_peer_uuid_uindex
+        unique (peer_uuid) comment 'The Peer UUID unique Index',
+    constraint authentication_otp_registered_peers_uuid_fk
         foreign key (peer_uuid) references peers (uuid)
             on update cascade on delete cascade
 )
-    comment 'Table for housing authentication passwords for registered peers';
+    comment 'Table for housing encrypted OTP secrets for for verification';
 
-create index authentication_passwords_updated_index
-    on authentication_passwords (updated)
-    comment 'The index of the of the updated column of the record';
+create index authentication_otp_updated_index
+    on authentication_otp (updated)
+    comment 'The index for the updated column';
 
--- src/Socialbox/Classes/Resources/database/authentication_passwords.sql
+++ src/Socialbox/Classes/Resources/database/authentication_passwords.sql
@@ -7,7 +7,7 @@ create table authentication_passwords
     constraint authentication_passwords_peer_uuid_uindex
         unique (peer_uuid) comment 'The primary unique index of the peer uuid',
     constraint authentication_passwords_registered_peers_uuid_fk
-        foreign key (peer_uuid) references registered_peers (uuid)
+        foreign key (peer_uuid) references peers (uuid)
             on update cascade on delete cascade
 )
     comment 'Table for housing authentication passwords for registered peers';
