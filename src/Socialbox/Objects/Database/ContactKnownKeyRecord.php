bdd35dc8ac49ccb96f64e00fccc33215e2ad03a8
netkas <netkas@n64.cc>
2025-01-29 15:23:42 -0500

    Added ContactKnownKeyRecord.php

---
+++ src/Socialbox/Objects/Database/ContactKnownKeyRecord.php
@@ -0,0 +1,25 @@
+<?php
+
+    namespace Socialbox\Objects\Database;
+
+    use Socialbox\Interfaces\SerializableInterface;
+
+    class ContactKnownKeyRecord implements SerializableInterface
+    {
+
+        /**
+         * @inheritDoc
+         */
+        public static function fromArray(array $data): object
+        {
+            // TODO: Implement fromArray() method.
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public function toArray(): array
+        {
+            // TODO: Implement toArray() method.
+        }
+    }
