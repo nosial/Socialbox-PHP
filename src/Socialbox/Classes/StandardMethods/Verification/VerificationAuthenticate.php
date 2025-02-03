cf1bd2e0570aa78ad2a024432d9d0cf814848fb0
netkas <netkas@n64.cc>
2025-02-03 17:53:39 -0500

    Renamed Authenticate to VerificationAuthenticate

--- src/Socialbox/Classes/StandardMethods/Verification/VerificationAuthenticate.php
+++ src/Socialbox/Classes/StandardMethods/Verification/VerificationAuthenticate.php
@@ -12,7 +12,7 @@
     use Socialbox\Objects\ClientRequest;
     use Socialbox\Objects\RpcRequest;
 
-    class Authenticate extends Method
+    class VerificationAuthenticate extends Method
     {
         /**
          * @inheritDoc
