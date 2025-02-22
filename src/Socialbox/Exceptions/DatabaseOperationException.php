1f5a04d04eb52194a9802cce9a3ab43c6f724024
netkas <netkas@n64.cc>
2025-02-21 22:58:04 -0500

    Updated PhpDoc

--- src/Socialbox/Exceptions/DatabaseOperationException.php
+++ src/Socialbox/Exceptions/DatabaseOperationException.php
@@ -7,6 +7,12 @@
 
     class DatabaseOperationException extends Exception
     {
+        /**
+         * DatabaseOperationException constructor.
+         *
+         * @param string $message
+         * @param Throwable|null $throwable
+         */
         public function __construct(string $message, ?Throwable $throwable=null)
         {
             parent::__construct($message, 500, $throwable);
