fcabcd91dc2185f770c60255007b7e05859adde3
netkas <netkas@n64.cc>
2025-03-06 15:19:30 -0500

    Add EncryptionGetIncomingChannels class to retrieve incoming encryption channels with pagination support

---
+++ src/Socialbox/Classes/StandardMethods/EncryptionChannel/EncryptionGetIncomingChannels.php
@@ -0,0 +1,60 @@
+<?php
+
+    namespace Socialbox\Classes\StandardMethods\EncryptionChannel;
+
+    use Socialbox\Abstracts\Method;
+    use Socialbox\Classes\Configuration;
+    use Socialbox\Classes\Validator;
+    use Socialbox\Enums\StandardError;
+    use Socialbox\Exceptions\DatabaseOperationException;
+    use Socialbox\Exceptions\Standard\InvalidRpcArgumentException;
+    use Socialbox\Exceptions\Standard\MissingRpcArgumentException;
+    use Socialbox\Exceptions\Standard\StandardRpcException;
+    use Socialbox\Interfaces\SerializableInterface;
+    use Socialbox\Managers\EncryptionChannelManager;
+    use Socialbox\Objects\ClientRequest;
+    use Socialbox\Objects\RpcRequest;
+
+    class EncryptionGetIncomingChannels extends Method
+    {
+
+        /**
+         * @inheritDoc
+         */
+        public static function execute(ClientRequest $request, RpcRequest $rpcRequest): ?SerializableInterface
+        {
+            if(!$rpcRequest->containsParameter('channel_uuid'))
+            {
+                throw new MissingRpcArgumentException('channel_uuid');
+            }
+            elseif(!Validator::validateUuid($rpcRequest->getParameter('channel_uuid')))
+            {
+                throw new InvalidRpcArgumentException('channel_uuid', 'The given channel uuid is not a valid UUID V4');
+            }
+
+            $page = 1;
+            $limit = Configuration::getPoliciesConfiguration()->getEncryptionChannelIncomingLimit();
+            if($rpcRequest->containsParameter('page'))
+            {
+                $page = (int)$rpcRequest->getParameter('page');
+            }
+
+            if($rpcRequest->containsParameter('limit'))
+            {
+                $limit = (int)$rpcRequest->getParameter('limit');
+            }
+
+            try
+            {
+                $requestingPeer = $request->getPeer();
+                return $rpcRequest->produceResponse(array_map(function($channel) use ($requestingPeer)
+                {
+                    return $channel->toStandard();
+                }, EncryptionChannelManager::getIncomingChannels($requestingPeer->getAddress(), $page, $limit)));
+            }
+            catch(DatabaseOperationException $e)
+            {
+                throw new StandardRpcException('There was an error while trying to obtain incoming encryption channels', StandardError::INTERNAL_SERVER_ERROR, $e);
+            }
+        }
+    }
